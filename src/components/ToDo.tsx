8bafcdd6f34920e92fa2481a26a164e52984cd55
byun970 <byun970@gmail.com>
2023-11-01 16:45:35 +0900

    recoil selector

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { IToDo } from "./atoms";
+import { Categories, IToDo, toDoState } from "./atoms";
 import { styled } from "styled-components";
+import { useSetRecoilState } from "recoil";
 
 const Li = styled.li`
   margin: 5px 0px;
@@ -10,7 +11,7 @@ const Li = styled.li`
   border-radius: 5px;
   align-items: center;
   position: relative;
-  @media (max-width:768px) {
+  @media (max-width: 768px) {
     span {
       font-size: 1rem;
     }
@@ -18,7 +19,7 @@ const Li = styled.li`
       font-size: 1rem;
     }
   }
-  @media (min-width:768px) {
+  @media (min-width: 768px) {
     span {
       font-size: 1.5rem;
     }
@@ -42,14 +43,43 @@ const Button = styled.button`
   cursor: pointer;
 `;
 
-const ToDo = ({ text }: IToDo) => {
+const ToDo = ({ text, category, id }: IToDo) => {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: name as any };
+      console.log(oldToDo, newToDo);
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
   return (
     <Li>
       <span>{text}</span>
       <Buttons>
-        <Button color="#2ecc71">Doing</Button>
-        <Button color="#f1c40f">To Do</Button>
-        <Button color="#e74c3c">Done</Button>
+        {category !== Categories.DOING && (
+          <Button name={Categories.DOING} onClick={onClick} color="#2ecc71">
+            Doing
+          </Button>
+        )}
+        {category !== Categories.TO_DO && (
+          <Button name={Categories.TO_DO} onClick={onClick} color="#f1c40f">
+            To Do
+          </Button>
+        )}
+        {category !== Categories.DONE && (
+          <Button name={Categories.DONE} onClick={onClick} color="#e74c3c">
+            Done
+          </Button>
+        )}
       </Buttons>
     </Li>
   );
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,40 +1,46 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
-import { toDoState } from "./atoms";
+import { Categories, categoryState, toDoSelector } from "./atoms";
 import ToDo from "./ToDo";
 import styled from "styled-components";
 
-const ToDoList = () => {
-  const toDos = useRecoilValue(toDoState);
-
-  const ToDoWrapper = styled.div`
+const ToDoWrapper = styled.div`
+  h1 {
+    text-align: center;
+    margin: 20px 0px;
+  }
+  @media (max-width: 768px) {
     h1 {
-      text-align: center;
-      margin: 20px 0px;
+      font-size: 2rem;
     }
-    @media (max-width: 768px) {
-      h1 {
-        font-size: 2rem;
-      }
+  }
+  @media (min-width: 768px) {
+    h1 {
+      font-size: 3rem;
     }
-    @media (min-width: 768px) {
-      h1 {
-        font-size: 3rem;
-      }
-    }
-  `;
+  }
+`;
 
-
+const ToDoList = () => {
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value as any);
+  };
+  console.log(toDos);  
   return (
     <ToDoWrapper>
       <h1>To Do</h1>
+      <select value={category} onInput={onInput}>
+        <option value={Categories.TO_DO}>To Do</option>
+        <option value={Categories.DOING}>Doing</option>
+        <option value={Categories.DONE}>Done</option>
+      </select>
       <CreateToDo />
-      <ul>
-        {toDos.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
     </ToDoWrapper>
   );
 };
